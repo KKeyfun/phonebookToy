2c95b2177a7a4b03c259e8475dffc1edf8bb6805
kkeyfun <KKeyfun@users.noreply.github.com>
2023-05-02 22:48:07 -0700

    Added insert+find query functionality

--- mongo.js
+++ mongo.js
@@ -1,31 +1,86 @@
+// Standalone file for command line insert and find queries to mongo db
 const mongoose = require('mongoose');
+const people = require('./data/phonebook');
 
-if (process.argv.length<4) {
-  console.log('give username and password as argument')
-  process.exit(1)
-}
+
 const username = process.argv[2];
 const password = process.argv[3];
 
 const url =
-  `mongodb+srv://${username}:${password}@mongomon.aaimrtn.mongodb.net/noteApp?retryWrites=true&w=majority`
+  `mongodb+srv://${username}:${password}@mongomon.aaimrtn.mongodb.net/phonebookApp?retryWrites=true&w=majority`
 
 mongoose.set('strictQuery',false)
 mongoose.connect(url)
 
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
+const phonebookSchema = new mongoose.Schema({
+  name : String,
+  number: String,
 })
 
-const Note = mongoose.model('Note', noteSchema)
+const Entry = mongoose.model('Entries', phonebookSchema)
 
-const note = new Note({
-  content: 'HTML is Easy',
-  important: true,
-})
+const queryType = process.argv[4].toLowerCase();
 
-note.save().then(result => {
-  console.log('note saved!')
-  mongoose.connection.close()
-})
+if(queryType === 'insert'){
+    if(process.argv[5] && process.argv[6]){
+        addToDatabase([{
+            name:process.argv[5],
+            number:process.argv[6]}]
+        );
+    } else {
+        console.log('give username and password as argument')
+        process.exit(1)
+    }
+} else if(queryType === 'find'){
+    searchQuery(process.argv[5],process.argv[6]);
+}
+
+function addToDatabase(arr){
+    if(!arr){
+        return false;
+    }
+    if(arr.length===1){
+        const entry = new Entry({...arr[0]});
+        entry.save().then(result => {
+            console.log(`Added ${result.name} : ${result.number} to phonebook`);
+            mongoose.connection.close();
+        }).catch((error) => {
+            console.log(error);
+        })
+        
+    }else {
+        Entry.insertMany(people).then((result)=>{
+            console.log(result);
+            mongoose.connection.close();
+        }).catch((error) => {
+            console.log(error);
+        })
+    }
+}
+
+function searchQuery(field,value){
+    if(!field && !value){
+        Entry.find({}).then(result => {
+            result.forEach(entry => {
+                console.log(entry);
+            })
+            mongoose.connection.close();
+        })
+    } else if(!value){
+        Entry.find({}).select(`${field} -_id`).then(result => {
+            result.forEach(entry => {
+                console.log(entry);
+            })
+            mongoose.connection.close();
+        })
+    }else {
+        Entry.find({[`${field}`]:value}).then(result => {
+            result.forEach(entry => {
+                console.log(entry);
+            })
+            mongoose.connection.close();
+        })
+    }
+}
+
+// addToDatabase(people)
